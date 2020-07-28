4e44775ff42a2e2f08b6bb6322ef06c4eabab003
anthony keogh <siliconirish@gmal.com>
2020-07-28 14:24:03 +0100

    Commit message

--- shoopingapp/settings.py
+++ shoopingapp/settings.py
@@ -37,7 +37,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zara-grace.herokuapp.com']
 
 
 # Application definition
