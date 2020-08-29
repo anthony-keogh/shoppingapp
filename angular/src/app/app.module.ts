47a15b1849fb9c1c6d1a0e00f7812028b5a6ed6b
anthony keogh <siliconirish@gmal.com>
2020-08-29 11:32:52 +0100

    Changes to javascript

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProductService } from './product.service'
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,35 +1,4 @@
 
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.count = 0;
-});
 
-app.controller('myCtrl', function($scope) {
-    $scope.count2 = 0;
-});
-
-var app = angular.module('myApp').config(function($interpolateProvider) {
-    $interpolateProvider.startSymbol('{$');
-    $interpolateProvider.endSymbol('$}');
-});
-
-
-
-$('.likebutton').click(function(){
-    
-$.ajax(
-    {
-        type:"GET",
-        url: "/likeproduct",
-        dataType:'json',
-        success: function(data) 
-        {
-          console.log(JSON.stringify(data))  
-        },
-        error: function(request, status, error){
-            console.log("Error:"+ error)
-        }
-     });
-});
 
 
