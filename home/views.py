35d58573cd105572a62ac8a0f82a3ed0c1bc9688
anthony keogh <siliconirish@gmal.com>
2021-02-07 19:57:16 +0000

    Css index change

--- home/views.py
+++ home/views.py
@@ -3,5 +3,6 @@ from products.models import Product_item
 
 # Create your views here.
 def index(request):
-    
-    return render(request, 'index.html')
+    products = Product_item.objects.all()
+     
+    return render(request, 'index.html',  {'products':products})
